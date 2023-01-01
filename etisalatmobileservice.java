114cc212aab61eaf779696086a82bd7b15f95b4f
DanaMostafa48 <119539254+DanaMostafa48@users.noreply.github.com>
2023-01-01 17:23:30 +0200

    Add files via upload

---
+++ etisalatmobileservice.java
@@ -0,0 +1,14 @@
+package fawry;
+
+public class etisalatmobileservice extends mobilerechargeservice {
+	 @Override
+	    public void display() {
+	        System.out.println("Etisalat Mobile Recharge" );
+	    }
+
+	    @Override
+	    public void pay() {
+	        System.out.println("Etisalat Mobile Recharge Running pay ");
+	    }
+
+}

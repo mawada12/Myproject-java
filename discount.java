75f5e7f1154118045afd2712480e4c410cefa666
DanaMostafa48 <119539254+DanaMostafa48@users.noreply.github.com>
2023-01-01 17:21:36 +0200

    Add files via upload

---
+++ discount.java
@@ -0,0 +1,12 @@
+package fawry;
+
+public interface discount {
+	
+	   public void applyDiscount(services service);
+	    public String getDiscountName();
+	    public Double getDiscountAmount();
+
+	
+
+
+}
